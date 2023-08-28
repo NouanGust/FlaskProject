c46588d31ff244d2d6f93863f5107b9a9b118e90
Nohan Gustavo <nohan.gnb@gmail.com>
2023-08-28 19:51:07 -0300

    mudanças no projeto

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, flash
+import emoji
 
 app = Flask(__name__)
 app.secret_key = "Awsome_secret_key"
 
 @app.route("/hello")
 def index():
-    flash("Qual seu nome?")
+    flash("...")
+    flash('')
     return render_template("index.html")
 
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
-    flash("Olá, " + str(request.form['name_input']) + "! " + "Bom te conhecer :)")
+    if str(request.form['name_input']) != '':
+        flash("Saudações " + str(request.form['name_input']) + str(emoji.emojize(":vulcan_salute:")))
+        flash("Bom te conhecer!")
+    else:
+        flash("Tente digitar seu nome ou um nome qualquer. Não se preocupe, esse site não guarda seus dados.")
     return render_template('index.html')    
